659a7c2194f691259ad6d5a95e164d7070fe08cc
Alessandro Bacchini <allebacco@gmail.com>
2019-08-17 17:07:35 +0200

    Fix version name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='listthedocs',
-    version='2.0.0',
+    version='1.0.3',
     description='List your documentations',
     author='Alessandro Bacchini',
     author_email='allebacco@gmail.com',
